d87344ab51af5deb73057364cf81a3b573c3a896
José Rivera <105292665+Mckngbird@users.noreply.github.com>
2022-12-21 00:24:04 -0300

    Update MetodoPagoRestControllerTest.java

--- src/test/java/com/example/ing_soft/rest/MetodoPagoRestControllerTest.java
+++ src/test/java/com/example/ing_soft/rest/MetodoPagoRestControllerTest.java
@@ -2,15 +2,19 @@ package com.example.ing_soft.rest;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.example.ing_soft.model.MetodoPago;
 import com.example.ing_soft.service.MetodoPagoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ExtendWith(MockitoExtension.class)
 public class MetodoPagoRestControllerTest {
@@ -24,4 +28,20 @@ public class MetodoPagoRestControllerTest {
     private JacksonTester<List<MetodoPago>> jsonMetodoPagoList;
     private JacksonTester<List<Integer>> jsonIntegerList;
     
+    @BeforeEach
+    public void setup(){
+        JacksonTester.initFields(this,new ObjectMapper());
+        mockMvc = MockMvcBuilders.standaloneSetup(metodoPagoRestController).build();
+    }
+
+    @Test
+    public void siInvocoGetAllMetodoPagoYExistenMetodosPagoRetornarListaMetodoPagoYStatusOk() throws Exception{
+        // Arrage
+        MetodoPago metodoPago = getMetodoPago();
+        List<MetodoPago> metodoPagoList = List.of();
+        given(metodoPagoService.findAllMetodoPago()).willReturn(metodoPagoList);
+
+        // Act
+        MockHttpServletResponse response = mockMvc
+                .perform(MockMvcRequestBuilders.get("/metodoPago"))
 }
